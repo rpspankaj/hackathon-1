74e1c6923fe7e68b2e8eb850a209b1a8b79e2150
Roshan R <roshan.atl@gmail.com>
2016-10-17 15:28:06 +0530

    Update GroupDao.java

--- src/main/java/com/walmart/hackathon/persistence/GroupDao.java
+++ src/main/java/com/walmart/hackathon/persistence/GroupDao.java
@@ -1,11 +1,15 @@
 package com.walmart.hackathon.persistence;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.walmart.hackathon.model.UserGroup;
 
 public interface GroupDao extends JpaRepository<UserGroup,BigInteger>{
-
+	
+	@Query("select g from UserGroup g where groupId in( select u.groupId from GroupUserMapping u where u.userId=?1 )")
+	public List<UserGroup> findGroupsOfUser(BigInteger userId);
 }
